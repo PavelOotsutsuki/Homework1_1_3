97a34a6c83dee28f72dcbc22bf0f4d87be94dac1
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-27 21:26:15 +0300

    Добавьте файлы проекта.

---
+++ Homework1_1_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; // Имя пользователя
+            int age; // Возраст
+            string zodiac;// Знак зодиака
+            string job;// Работа
+
+            Console.Write("Как Вас зовут?: ");
+            name = Console.ReadLine();
+            Console.Write("Сколько Вам лет?: ");
+            age=Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Какой Ваш знак зодиака?: ");
+            zodiac=Console.ReadLine();
+            Console.Write("Где Вы работаете?: ");
+            job=Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Вас зовут " + name + ", вам " + age + " год, вы " + zodiac + " и работаете " + job);
+        }
+    }
+}
+
+
+
